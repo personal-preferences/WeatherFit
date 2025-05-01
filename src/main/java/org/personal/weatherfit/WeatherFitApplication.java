beff33c6be77ebace6efd677d593ac473dd6384f
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2025-05-01 15:09:00 +0900

    initial commit

---
+++ src/main/java/org/personal/weatherfit/WeatherFitApplication.java
@@ -0,0 +1,13 @@
+package org.personal.weatherfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/personal/weatherfit/WeatherFitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.weatherfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
